6c0d7174506e0f711ee41b8c49de892401640f6f
{} <hey@amegroups.com>
2024-08-09 17:08:35 +0800

    transferred

--- web/src/components/statisticsManager.tsx
+++ web/src/components/statisticsManager.tsx
@@ -4,7 +4,7 @@ export default function StatisticsManager(props:{totalFiles:number,totalSize:num
 
     return <>
         <div class="text-gray-400 mt-8 text-xl ">
-        We've already converted <span class="text-white text-2xl">{props.totalFiles}</span> files with a total size of <span class="text-white text-2xl">{formatBytes(props.totalSize)}</span>.
+        We've already transferred <span class="text-white text-2xl">{props.totalFiles}</span> files with a total size of <span class="text-white text-2xl">{formatBytes(props.totalSize)}</span>.
         </div>
     </>
 }
