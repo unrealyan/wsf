92453ec48da7bed6129c00a284ab55d7ccf0fd12
{} <hey@amegroups.com>
2024-08-19 14:24:15 +0800

    translate english

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -32,7 +32,7 @@ export default function Header(props: { store: any, setStore: any }) {
                         </svg>
                     </a>
 
-                    <a href="https://t.me/wsf009" target="_blank" class="telegram-icon" title="加入Telegram群组">
+                    <a href="https://t.me/wsf009" target="_blank" class="telegram-icon" title="Join Telegram Group and Provide Feedback">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M21.5 2l-18.5 9 6 3 9-7-7 9 11 4-0.5-18z"></path>
                         </svg>
